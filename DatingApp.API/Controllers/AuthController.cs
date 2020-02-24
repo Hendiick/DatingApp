039927a53238a29336c50c9cff9cba0340829f8f
Henrik Ojeda <henrik.ojeda@hotmail.com>
2020-02-24 23:18:11 +0100

    First Commit

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -50,6 +50,10 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            try
+            {
+                  throw new Exception("Computer sayts no!");
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
@@ -80,6 +84,11 @@ namespace DatingApp.API.Controllers
              return Ok(new {
                  token = tokenHandler.WriteToken(token)
              });
+
+            }catch {
+                return StatusCode(500, "Computer really says no!");
+            }
+          
         }
 
     }
